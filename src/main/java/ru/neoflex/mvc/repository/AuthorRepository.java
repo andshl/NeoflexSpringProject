dc1bea40750a37f790e5601cc79f99979da46bcf
andshl <andshl@mail.ru>
2018-12-11 20:14:08 +0400

    initial commit

---
+++ src/main/java/ru/neoflex/mvc/repository/AuthorRepository.java
@@ -0,0 +1,8 @@
+package ru.neoflex.mvc.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.neoflex.mvc.entity.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+    String findByName(String name);
+}
